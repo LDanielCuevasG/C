55a297492141137de567a8414b220e8cbd820fed
LDanielCuevasG <38821622+LDanielCuevasG@users.noreply.github.com>
2018-05-15 18:24:49 -0500

    Add files via upload

---
+++ util.h
@@ -0,0 +1,28 @@
+//Prototipos de aplicacion
+void menuSelect(void);
+void Registrar(void);
+void Abrir(void);
+void Creditos(void);
+void Lista(char [500][100], int, int);
+void AbrirBusqueda(char [500][100]);
+void AbrirVisita(char [500][100]);
+void AbrirModificar(char [500][100]);
+int asignacionID(void);
+void BorrarAbrir(int);
+void GetData(char [500][100]);
+void SetData(char [500][100]);
+
+
+
+//Prototipos de la interfaz
+void menuTitulo(void);
+void menuInterface(int);
+void InterfazRegistrar(int, int, int);
+void OpcionesRegistrar(int);
+void OpcionesAbrir(int);
+void OpcionesModificar(int);
+void MovimientoPagina(int);
+void MovimientoModificar( char [20],char [20],char [20],char [20],int);
+
+
+
